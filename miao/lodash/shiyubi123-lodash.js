a8f7336780c0c96264b624c79c34f46dd291cbdb
shiyubi123 <470947999@qq.com>
2019-07-23 21:17:31 +0800

    flatten

--- miao/lodash/shiyubi123-lodash.js
+++ miao/lodash/shiyubi123-lodash.js
@@ -180,7 +180,7 @@ var shiyubi123 = {
         return result
     },
 
-    head: array => array[0] ? array[0] : null, 
+    head: array => array[0], 
     
     indexOf : function indexOf(array, value, fromIndex = 0){
             for(var i = fromIndex;i < array.length;i++) {
