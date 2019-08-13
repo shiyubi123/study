19c5cf61125def6c0cc5384d707689d179c6a38b
shiyubi123 <470947999@qq.com>
2019-08-13 11:29:00 +0800

    dropwhile

--- miao/lodash/shiyubi123-2.js
+++ miao/lodash/shiyubi123-2.js
@@ -1,5 +1,5 @@
-var shiyubi123 = {
-    chunk: function chunk (array, size = 1) {
+
+    function chunk (array, size = 1) {
                 var  newary = []
                 for (var i = 0;i + size - 1 < array.length;i += size) {
                     newary.push(array.slice(i,i + size))
@@ -8,13 +8,13 @@ var shiyubi123 = {
                     newary.push(array.slice(i))
                 }
                 return newary
-            },
+            }
     
-    compact: function compact(array) {
+    function compact(array) {
         return array.filter(it => it)
-    },
+    }
     
-    concat: function concat (array, values) {
+    function concat (array, values) {
         debugger
         var newary = []
         for (var i = 0;i < arguments.length;i++) {
@@ -25,9 +25,9 @@ var shiyubi123 = {
             }
         }
         return newary
-    },
+    }
     
-    difference: function difference (array, ...values) {
+    function difference (array, ...values) {
         debugger
         var dif = []
         var map = {}
@@ -42,17 +42,14 @@ var shiyubi123 = {
             }
         }
         return dif
-    },
+    }
     
-    differenceBy: function differenceBy (array, values,comparator) {
+    function differenceBy (array, values,comparator) {
        
-    },
-    
-    drop: (array, n = 1) => array.slice(n),
-    
-    dropRight: (array, n = 1) => array.length > n ? array.slice(0,array.length - n) : [],
+    }
 
-    dropRightWhile: function dropRightWhile(array,predicate = identity()) {
+
+    function dropRightWhile(array,predicate = identity()) {
         debugger
         predicate = iteratee(predicate)
         for(var i = array.length - 1;i >= 0;i--){
@@ -63,42 +60,41 @@ var shiyubi123 = {
             }
         }
         return array
-    },
+    }
     
-    dropWhile: function dropWhile(array,predicate = identity()) {
+    function dropWhile(array,predicate = identity()) {
         predicate = iteratee(predicate)
         for(var i = 0;i < array.length;i++){
-            if(predicate(array[i])){
+            if(!predicate(array[i])){
                 array = array.slice(1)
             }
         }
         return array
-    },
-
-    fill: function fill(array, value, start = 0, end = array.length){
+    }
+    function fill(array, value, start = 0, end = array.length){
             for(var i = start;i < end;i++) {
                 array[i] = value
             }
             return array
-        },
+        }
     
-    findIndex: function findIndex(array, predicate = identity(value), fromIndex = 0){
+    function findIndex(array, predicate = identity(value), fromIndex = 0){
                 for(var i = fromIndex;i < array.length;i++) {
                     if (predicate(array[i],i,array)){
                         return i
                     }
                 }
-            },
+            }
     
-    findLastIndex: function findLastIndex(array, predicate = identity(value), fromIndex = array.length-1){
+    function findLastIndex(array, predicate = identity(value), fromIndex = array.length-1){
                     for(var i = fromIndex;i >= 0;i--) {
                         if (predicate(array[i],i,array)){
                             return i
                         }
                     }   
-                },
+                }
 
-    flatten: function flatten (array) {
+    function flatten (array) {
         var result = []
         for(item of array){
             if(Array.isArray(item)){
@@ -110,9 +106,9 @@ var shiyubi123 = {
             }
         }
         return result
-    },
+    }
 
-    flattenDeep: function flattenDeep (array){
+    function flattenDeep (array){
         debugger
         var result = []
         for(var i = 0;i < array.length;i++) {
@@ -124,11 +120,11 @@ var shiyubi123 = {
             }
         }
         return result
-    },
+    }
     
     
 
-    flattenDepth: function flattenDepth (array,depth = 1){
+    function flattenDepth (array,depth = 1){
         debugger
         var result = []
         if(depth == 0){
@@ -144,30 +140,28 @@ var shiyubi123 = {
             }
         }
         return result
-    },
+    }
     
-    fromPairs: function fromPairs(pairs) {
+    function fromPairs(pairs) {
         var result = {}
         for(var i = 0;i < pairs.length;i++) {
             result[pairs[i][0]] = pairs[i][1]
         }
         return result
-    },
+    }
 
-    head: array => array[0], 
     
-    indexOf : function indexOf(array, value, fromIndex = 0){
+    function indexOf(array, value, fromIndex = 0){
             for(var i = fromIndex;i < array.length;i++) {
                 if(array[i] == value){
                     return i
                 }
             }
             return -1
-        },
+        }
 
-    initial: array => array.slice(0,array.length - 1),
 
-    intersection: function intersection(...arrays) {
+    function intersection(...arrays) {
             debugger
             var result = []
             var map = {}
@@ -186,34 +180,32 @@ var shiyubi123 = {
                 }
             }
             return result
-        },
+        }
     
-    join: (array, separator = ',') => array.reduce((a,b) => a + '' + separator + '' + b),
     
-    last: array => array[array.length - 1],
 
-    lastIndexOf: function lastIndexOf(array, value, fromIndex = array.length-1){
+    function lastIndexOf(array, value, fromIndex = array.length-1){
                 for(var i = fromIndex;i >= 0;i--){
                     if(array[i] == value){
                         return i
                     }
                 }
                 return -1
-            },
+            }
 
-    pull: function pull(array, ...values){
+    function pull(array, ...values){
         var map = {}
             for(var i = 0;i < values.length;i++) {
                 map[values[i]] = 1
             }
         return array.filter((a,values) => !(a in map))
-    },
+    }
     
-    isArray: function isArray (value) {
+    function isArray (value) {
         return Object.prototype.toString.apply(value) == 'object Array'
-    },
+    }
 
-    reverse: function reverse(array) {
+    function reverse(array) {   
             var len = array.length - 1
             var halflen = Math.floor(array.length / 2)
             for(var i = 0;i < halflen;i++){
@@ -222,9 +214,9 @@ var shiyubi123 = {
                 array[len - i] = buffer
             }
             return array
-        },
+        }
     
-    sortedIndex: function sortedIndex(array, value){
+    function sortedIndex(array, value){
         var low = 0
         var high = array.length - 1
         var mid = Math.floor((low + high) / 2)
@@ -244,9 +236,9 @@ var shiyubi123 = {
             }
         }
         return mid
-    },
+    }
 
-    bind: function bind(f,thisArgs,...fixedArgs){
+    function bind(f,thisArgs,...fixedArgs){
         return function(...args){
             var actualArgs = [...fixedArgs]
             for(var i = 0;i < actualArgs.length;i++){
@@ -257,9 +249,9 @@ var shiyubi123 = {
             actualArgs.push(...args)
             return f.apply(thisArgs,actualArgs)
         }
-    },
+    }
 
-    isMatch: function isMatch(obj,src){
+    function isMatch(obj,src){
         if(obj === src){
             return true
         }
@@ -273,17 +265,17 @@ var shiyubi123 = {
             }
         }
         return true
-    },
+    }
 
-    matches: function matches(src){
+    function matches(src){
         return bind(isMatch,null,window,src)
-    },
+    }
 
-    toPath:function toPath(path){
+    function toPath(path){
         return path.split(/\.|\[|\]./g)
-    },
+    }
 
-    get: function get(obj,path,defaultVal = false){
+    function get(obj,path,defaultVal = false){
         var path = toPath(path)
         for(var i = 0;i < path.length;i++){
             if(obj === undefined){
@@ -292,7 +284,7 @@ var shiyubi123 = {
             obj = obj[path[i]]
         }
         return obj
-    },//也可以用递归，每次obj[path[0]]，然后调path.slice(1),但是前提是要先toPath
+    }//也可以用递归，每次obj[path[0]]，然后调path.slice(1),但是前提是要先toPath
     
     //function get(obj,path,defaultVal){
         //var path = toPath(path)
@@ -301,21 +293,13 @@ var shiyubi123 = {
         //},obj)
     //}
 
-    matchesProperty: function matchesProperty(path,value){
+    function matchesProperty(path,value){
         return function (obj){
             return isEqual(get(obj,path),value)
         }
-    }, 
+    }
 
-    identity: function identity(value){
-                return arguments[0]
-    },
-    
-    property: function property(path){
-        return bind(get,null,window,path)
-    },
-
-    isEqual: function isEqual(value, other){
+    function isEqual(value, other){
         debugger
         if(typeof value == 'object' || typeof value == 'array'){
             if(Object.keys(value).length != Object.keys(other).length){
@@ -332,13 +316,22 @@ var shiyubi123 = {
             return value == other
         }
         return true
-    },
+    }
 
-    iteratee: function(value){
+    function identity(value){
+                return arguments[0]
+    }
+    
+    function property(path){
+        return bind(get,null,window,path)
+    }
+
+    function iteratee(value){
+        debugger
         if(typeof value == 'string') {
             return property(value)
         }
-        if(typeof Array.isArray(value) == true){
+        if(Array.isArray(value) == true){
             return matchesProperty(value[0],value[1])
         }
         if(typeof value == 'object') {
@@ -346,5 +339,3 @@ var shiyubi123 = {
         }
         return value
     }//这个iteratee方法的任务就是把一个数组，一个对象，或者一个字符串变成一个有效的function来遍历数组或对象找到符合要求的属性
-    ,
-}
